0de9b524217c7c052f8dad72cd316f9b5e7edd7d
ehopealot <erikjhope@gmail.com>
2013-02-27 13:58:47 -0600

    Initial Commit

---
+++ EJHStretchyTableViewHeaderDemo/EJHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  EJHAppDelegate.h
+//  EJHStretchyTableViewHeaderDemo
+//
+//  Created by Erik Hope on 2/27/13.
+//  Copyright (c) 2013 ehope. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EJHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ EJHStretchyTableViewHeaderDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  EJHStretchyTableViewHeaderDemo
+//
+//  Created by Erik Hope on 2/27/13.
+//  Copyright (c) 2013 ehope. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "EJHAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([EJHAppDelegate class]));
+    }
+}
